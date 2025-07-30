b85231b31c1360dbc8c8fcc6519101218cf9ffdc
Joseph <josephpajuelo7@gmail.com>
2025-07-30 02:42:49 +0200

    Inicio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { Contact } from './pages/contact/contact';
+import { ProductDetail } from './pages/product-detail/product-detail';
+
+export const routes: Routes = [
+    {path:'',component: Home},
+    {path:'products', component: Products},
+    {path:'contact', component: Contact},
+    {path:'products/:id', component: ProductDetail},
+    {path:'**', redirectTo:'', pathMatch:'full'}
+];
